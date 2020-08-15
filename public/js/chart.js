185d8d0a78d04427052090448a68307b9ebb3af6
Paula1997 <ana.posadasr97@gmail.com>
2020-08-14 22:48:03 -0600

    cambios Ana

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,32 +1,93 @@
-const dispertionGraph = document.getElementById("dispertion-graph");
-const linearGraph = document.getElementById("linear-graph");
+// const dispertionGraph = document.getElementById("dispertion-graph");
+// const linearGraph = document.getElementById("linear-graph");
 
-const linearChart = new Chart(linearGraph, {
-  type: 'line',
-  data: [0,10,20,30],
-  options: {
-    responsive: true
-  }
-})
+// const linearChart = new Chart(linearGraph, {
+//   type: 'line',
+//   data: [0,10,20,30],
+//   options: {
+//     responsive: true
+//   }
+// })
 
-const dispertionChart = new Chart(dispertionGraph,{
-  type: 'scatter',
-  data: {
-    datasets: [{
-      label: 'Scatter Dataset',
-      data: [{
-          x: -10,
-          y: 0
-      }, {
-          x: 0,
-          y: 10
-      }, {
-          x: 10,
-          y: 5
-      }]
-  }]
-  },
-  options: {
-    responsive: true
-  }
-})
+// const dispertionChart = new Chart(dispertionGraph,{
+//   type: 'scatter',
+//   data: {
+//     datasets: [{
+//       label: 'Nacimientos', 
+//       borderColor: 'Green',
+//       backgroundColor: 'Green',
+      
+//       data: [{
+//           x: -10,
+//           y: 4
+//       }, {
+//           x: 5,
+//           y: 6
+//       }, {
+//           x: 10,
+//           y: 3
+//       },
+//     ]
+//   },
+//   {
+//     label: 'Muertes', 
+//     borderColor: 'Red',
+//     backgroundColor: 'Red',
+    
+//     data: [{
+//         x: -10,
+//         y: 0
+//     }, {
+//         x: 0,
+//         y: 10
+//     }, {
+//         x: 10,
+//         y: 5
+//     },
+//   ]
+// },
+// {
+//   label: 'Migrantes', 
+//   borderColor: 'Blue',
+//   backgroundColor: 'Blue',
+//   // pointBackgroundColor:['blue','green','red'],
+//   data: [{
+//       x: -10,
+//       y: 0
+//   }, {
+//       x: 0,
+//       y: 10
+//   }, {
+//       x: 10,
+//       y: 5
+//   },
+// ]
+// },
+// {
+//   label: 'Inmigrantes', 
+//   borderColor: 'purple',
+//   backgroundColor: 'purple',
+  
+//   data: [{
+//       x: -10,
+//       y: 0
+//   }, {
+//       x: 0,
+//       y: 10
+//   }, {
+//       x: 10,
+//       y: 5
+//   },
+// ]
+// }]
+//   },
+//   options: {
+//     responsive: true,
+//     scales: {
+//       xAxes: [{
+//           type: 'linear',
+//           position: 'bottom'
+//       }]
+//   }
+//   }
+// })
